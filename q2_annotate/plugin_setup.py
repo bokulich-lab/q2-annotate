b8648b144ab220ee36aef73a1223fa361b1db149
Colin Wood <woodcolin38@gmail.com>
2025-03-18 15:32:03 -0700

    first methods for merging kraken2 reports & outpus

--- q2_annotate/plugin_setup.py
+++ q2_annotate/plugin_setup.py
@@ -167,8 +167,13 @@ plugin.methods.register_function(
 )
 
 T_kraken_in, T_kraken_out_rep, T_kraken_out_hits = TypeMap({
-    List[SampleData[SequencesWithQuality |
-               PairedEndSequencesWithQuality | JoinedSequencesWithQuality]]: (
+    List[
+        SampleData[
+            SequencesWithQuality |
+            PairedEndSequencesWithQuality |
+            JoinedSequencesWithQuality
+        ]
+    ]: (
         SampleData[Kraken2Reports % Properties('reads')],
         SampleData[Kraken2Outputs % Properties('reads')]
     ),
