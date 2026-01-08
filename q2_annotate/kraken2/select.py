6caf40ec20fd182309004044ce880c7ec304895d
Michal Ziemski <mziemski@ethz.ch>
2026-01-08 16:23:47 +0100

    Lint

--- q2_annotate/kraken2/select.py
+++ q2_annotate/kraken2/select.py
@@ -5,7 +5,6 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-import biom
 import glob
 import os
 from collections import deque
