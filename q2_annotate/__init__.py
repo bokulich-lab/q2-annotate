78fa73a69a9a44ffed52fb872d6cdc4a711d0f86
Michal Ziemski <mziemski@ethz.ch>
2026-06-05 11:17:52 +0200

    MAINT: move multiply-tables action to q2-feature-table

--- q2_annotate/__init__.py
+++ q2_annotate/__init__.py
@@ -19,13 +19,7 @@ from .kraken2 import (
     helpers as kraken_helpers,
 )
 from .metabat2 import metabat2
-from ._utils import (
-    get_feature_lengths,
-    multiply_tables,
-    _multiply_tables,
-    _multiply_tables_pa,
-    _multiply_tables_relative,
-)
+from ._utils import get_feature_lengths
 
 try:
     from ._version import __version__
@@ -47,10 +41,6 @@ __all__ = [
     "filter_derep_mags",
     "filter_mags",
     "get_feature_lengths",
-    "multiply_tables",
-    "_multiply_tables",
-    "_multiply_tables_pa",
-    "_multiply_tables_relative",
     "abundance",
     "filter_reads_pangenome",
 ]
